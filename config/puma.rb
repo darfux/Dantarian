92ac334d0d5ce4d626ef8d916e1dc31570b9c08e
darfux <darfux@163.com>
2016-10-28 10:39:29 +0800

    For deployment

--- config/puma.rb
+++ config/puma.rb
@@ -9,11 +9,14 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
+bind 'unix:///home/darfux/Project/Lab/Dantarian/tmp/puma.sock'
+
+pidfile 'tmp/pids/puma.pid'
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
